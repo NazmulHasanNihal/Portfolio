9480ee08a6a4b6b6784410d808e34290b82188ff
MD Nazmul <nazmulhas36@gmail.com>
2024-10-29 20:06:44 +0600

    updated

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,15 +1,29 @@
 const STORAGE_KEY = "theme";
-const THEME_ATTR = "data-theme";
-const QUERY_KEY = "(prefers-color-scheme: dark)";
-const themes = { LIGHT: "light", DARK: "dark" };
+const THEME_ATTR  = "data-theme";
+const QUERY_KEY   = "(prefers-color-scheme: dark)";
+
+const themes = {
+  LIGHT: "light",
+  DARK: "dark",
+};
 
 initTheme();
 
 function initTheme() {
   const savedTheme = localStorage.getItem(STORAGE_KEY);
-  const systemTheme = window.matchMedia && window.matchMedia(QUERY_KEY).matches ? themes.DARK : themes.LIGHT;
-  setTheme(savedTheme || systemTheme);
 
+  if (savedTheme) {
+    // Storage theme
+    setTheme(savedTheme);
+  } else if (window.matchMedia && window.matchMedia(QUERY_KEY).matches) {
+    // system theme
+    setTheme(themes.DARK);
+  } else {
+    // Default theme
+    setTheme(themes.LIGHT);
+  }
+
+  // Watch for system theme changes
   window.matchMedia(QUERY_KEY).addEventListener("change", (e) => {
     const newTheme = e.matches ? themes.DARK : themes.LIGHT;
     setTheme(newTheme);
@@ -17,7 +31,8 @@ function initTheme() {
 }
 
 function toggleTheme() {
-  const newTheme = getTheme() === themes.DARK ? themes.LIGHT : themes.DARK;
+  const theme = getTheme();
+  const newTheme = theme === themes.DARK ? themes.LIGHT : themes.DARK;
   setTheme(newTheme);
   localStorage.setItem(STORAGE_KEY, newTheme);
 }
@@ -28,7 +43,4 @@ function getTheme() {
 
 function setTheme(value) {
   document.documentElement.setAttribute(THEME_ATTR, value);
-  const themeIcon = document.getElementById("theme-icon");
-  themeIcon.classList.toggle("fa-sun", value === themes.DARK);
-  themeIcon.classList.toggle("fa-moon", value === themes.LIGHT);
 }
